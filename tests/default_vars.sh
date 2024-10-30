c0dd7eaecc3ef1703348f81fe7543dbafc9ef1da
denise.worthen <Denise.Worthen@noaa.gov>
2024-10-30 15:49:13 -0400

    set default rearranger to box
    * to resolve failures on gaea, the default ww3 rearranger is set
    as box on all platforms. All tests run with current resources w/
    slight bump for the debug_pdlib case. A followup issue will be
    created for the rearranger failure on Gaea and Gaea SAs will be
    contacted.

--- tests/default_vars.sh
+++ tests/default_vars.sh
@@ -1168,7 +1168,7 @@ export_ww3() {
   export WW3_PIO_FORMAT='pnetcdf'
   export WW3_PIO_STRIDE=4
   export WW3_PIO_IOTASKS=-99
-  export WW3_PIO_REARR='subset'
+  export WW3_PIO_REARR='box'
   export WW3_PIO_ROOT=-99
 }
 
@@ -1614,7 +1614,7 @@ export_hafs_regional ()
   export WW3_PIO_FORMAT='pnetcdf'
   export WW3_PIO_STRIDE=4
   export WW3_PIO_IOTASKS=-99
-  export WW3_PIO_REARR='subset'
+  export WW3_PIO_REARR='box'
   export WW3_PIO_ROOT=-99
 
   # Set CMEPS component defaults
