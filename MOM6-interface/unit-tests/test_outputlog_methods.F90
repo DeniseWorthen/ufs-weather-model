dbb8e8bec12af9e1dacf8b724d3a42b24bb30b9a
denise.worthen <Denise.Worthen@noaa.gov>
2026-07-01 08:25:36 -0600

    update tests and modules

--- MOM6-interface/unit-tests/test_outputlog_methods.F90
+++ MOM6-interface/unit-tests/test_outputlog_methods.F90
@@ -1,6 +1,6 @@
 program test_outputlog_methods
 
-  use mom_outputlog_methods, only : setrequest, settype, setrootname
+  use mom_outputlog_methods, only : setrequest, settype, setprefix
 
   implicit none
 
@@ -9,10 +9,10 @@ program test_outputlog_methods
 
   integer :: validfreqs(nfreq) = (/1, 3, 6, 24/)
 
-  integer           :: output_fh(nfreq)
-  character(len=12) :: output_type(nfreq)
-  character(len=12) :: output_rootname(nfreq)
-  character(len=24) :: overlength_rootname(nfreq)
+  integer           :: nml_fh(nfreq)
+  character(len=12) :: nml_type(nfreq)
+  character(len=12) :: nml_fnameprefix(nfreq)
+  character(len=24) :: overlength_fnameprefix(nfreq)
   character(len=34) :: longrootname
 
   logical           :: requested(nfreq)
@@ -35,9 +35,9 @@ program test_outputlog_methods
   ! ===========================================================================
 #ifdef test
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: outputfh==0 disables logging'
-  output_fh = (/0,0,0,0/)
-  requested = setrequest(validfreqs, output_fh, errmsg, ierr)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: outputfh==0 disables logging :'
+  nml_fh = (/0,0,0,0/)
+  requested = setrequest(validfreqs, nml_fh, errmsg, ierr)
 
   is_passing = (ierr == 0 .and. .not. any(requested))
   if (is_passing) then
@@ -50,9 +50,9 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: map request to canonical order'
-  output_fh = (/6,0,0,0/)
-  requested = setrequest(validfreqs, output_fh, errmsg, ierr)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: map request to canonical order :'
+  nml_fh = (/6,0,0,0/)
+  requested = setrequest(validfreqs, nml_fh, errmsg, ierr)
 
   is_passing = (ierr == 0 .and. requested(3) .and. .not. any(requested((/1,2,4/))))
   if (is_passing) then
@@ -65,9 +65,9 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: map request to canonical order'
-  output_fh = (/0,24,0,1/)
-  requested = setrequest(validfreqs, output_fh, errmsg, ierr)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: map request to canonical order :'
+  nml_fh = (/0,24,0,1/)
+  requested = setrequest(validfreqs, nml_fh, errmsg, ierr)
 
   is_passing = (ierr == 0 .and. requested(1) .and. requested(4) .and. .not. any(requested((/2,3/))))
   if (is_passing) then
@@ -80,9 +80,9 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: invalid frequency blocked'
-  output_fh = (/18,0,0,0/)
-  requested = setrequest(validfreqs, output_fh, errmsg, ierr)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: invalid frequency blocked :'
+  nml_fh = (/18,0,0,0/)
+  requested = setrequest(validfreqs, nml_fh, errmsg, ierr)
 
   is_passing = (ierr /= 0)
   if (is_passing) then
@@ -95,9 +95,9 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: duplicate frequencies blocked'
-  output_fh = (/24,24, 0, 0/)
-  requested = setrequest(validfreqs, output_fh, errmsg, ierr)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setrequest: duplicate frequencies blocked :'
+  nml_fh = (/24,24, 0, 0/)
+  requested = setrequest(validfreqs, nml_fh, errmsg, ierr)
 
   is_passing = (ierr /= 0)
   if (is_passing) then
@@ -113,12 +113,12 @@ program test_outputlog_methods
   ! ===========================================================================
 
   nt = nt+1
-  write(testname,'(A,I2.2,A)')'test ',nt,' settype: lower-case strings map correctly'
-  output_fh = (/1,6,0,0/)
+  write(testname,'(A,I2.2,A)')'test ',nt,' settype: lower-case strings map correctly :'
+  nml_fh = (/1,6,0,0/)
   requested = (/.true., .false., .true., .false./)
-  output_type = (/ character(len=12) :: 'none', 'average', '', '' /)
+  nml_type = (/ character(len=12) :: 'none', 'average', '', '' /)
 
-  timereduce = settype(validfreqs, requested, output_fh, output_type, errmsg, ierr)
+  timereduce = settype(validfreqs, requested, nml_fh, nml_type, errmsg, ierr)
 
   is_passing = (ierr == 0 .and. trim(timereduce(1)) == 'none' .and. trim(timereduce(2)) == 'average')
   if (is_passing) then
@@ -131,12 +131,12 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' settype: upper-case strings not allowed'
-  output_fh = (/3,24,0,0/)
+  write(testname,'(A,I2.2,A)')'test ',nt,' settype: upper-case strings not allowed :'
+  nml_fh = (/3,24,0,0/)
   requested = (/.false., .true., .false., .true./)
-  output_type = (/ character(len=12) :: '', 'NONE', '', 'AVERAGE' /)
+  nml_type = (/ character(len=12) :: '', 'NONE', '', 'AVERAGE' /)
 
-  timereduce = settype(validfreqs, requested, output_fh, output_type, errmsg, ierr)
+  timereduce = settype(validfreqs, requested, nml_fh, nml_type, errmsg, ierr)
 
   is_passing = (ierr /= 0)
   if (is_passing) then
@@ -149,12 +149,12 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' settype: invalid string or typo'
-  output_fh = (/3,24,0,0/)
-  output_type = (/ character(len=12) :: '', 'snapshot', '', 'avg' /)
+  write(testname,'(A,I2.2,A)')'test ',nt,' settype: invalid string or typo :'
+  nml_fh = (/3,24,0,0/)
+  nml_type = (/ character(len=12) :: '', 'snapshot', '', 'avg' /)
   requested = (/.false., .true., .false., .true./)
 
-  timereduce = settype(validfreqs, requested, output_fh, output_type, errmsg, ierr)
+  timereduce = settype(validfreqs, requested, nml_fh, nml_type, errmsg, ierr)
 
   is_passing = (ierr /= 0)
   if (is_passing) then
@@ -167,12 +167,12 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' settype: empty string on an active frequency defaults to average'
-  output_fh = (/1,6,0,0/)
+  write(testname,'(A,I2.2,A)')'test ',nt,' settype: empty string on an active frequency defaults to average :'
+  nml_fh = (/1,6,0,0/)
   requested = (/ .true., .false., .true., .false. /)
-  output_type = (/ character(len=12) :: 'none', '', '', '' /)
+  nml_type = (/ character(len=12) :: 'none', '', '', '' /)
 
-  timereduce = settype(validfreqs, requested, output_fh, output_type, errmsg, ierr)
+  timereduce = settype(validfreqs, requested, nml_fh, nml_type, errmsg, ierr)
 
   is_passing = (ierr == 0 .and. trim(timereduce(1)) == 'none' .and. trim(timereduce(3)) == 'average')
 
@@ -186,12 +186,12 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' settype: mis-aligned type for active frequency'
-  output_fh = (/1,6,0,0/)
+  write(testname,'(A,I2.2,A)')'test ',nt,' settype: mis-aligned type for active frequency :'
+  nml_fh = (/1,6,0,0/)
   requested = (/ .true., .false., .true., .false. /)
-  output_type = (/ character(len=12) :: 'none', 'average', '', '' /)
+  nml_type = (/ character(len=12) :: 'none', 'average', '', '' /)
 
-  timereduce = settype(validfreqs, requested, output_fh, output_type, errmsg, ierr)
+  timereduce = settype(validfreqs, requested, nml_fh, nml_type, errmsg, ierr)
 
   is_passing = (ierr /= 0)
   if (is_passing) then
@@ -203,17 +203,16 @@ program test_outputlog_methods
   endif
 
   ! ===========================================================================
-  ! test setrootname
+  ! test setprefix
   ! ===========================================================================
 
-  ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrootname: single request, blank rootname defaults to ocn'
-  output_fh = (/1,0,0,0/)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setprefix: single request, blank rootname defaults to ocn :'
+  nml_fh = (/1,0,0,0/)
   requested = (/ .true., .false., .false., .false. /)
-  output_rootname = (/ character(len=12) :: '', '', '', '' /) ! Active slot 1 is empty
+  nml_fnameprefix = (/ character(len=12) :: '', '', '', '' /) ! Active slot 1 is empty
 
-  fnameroot = setrootname(validfreqs, requested, output_fh, output_rootname, errmsg, ierr)
+  fnameroot = setprefix(validfreqs, requested, nml_fh, nml_fnameprefix, errmsg, ierr)
 
   is_passing = (ierr == 0 .and. trim(fnameroot(1)) == 'ocn')
   if (is_passing) then
@@ -226,14 +225,14 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrootname: multi-request, each must set rootname'
-  output_fh = (/1,3,0,0/)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setprefix: multi-request, each must set rootname :'
+  nml_fh = (/1,3,0,0/)
   requested = (/ .true., .true., .false., .false. /)
-  output_rootname = (/ character(len=12) :: 'ocn_1h', 'ocn_3h', '', '' /)
+  nml_fnameprefix = (/ character(len=12) :: 'ocn_01h', 'ocn_03h', '', '' /)
 
-  fnameroot = setrootname(validfreqs, requested, output_fh, output_rootname, errmsg, ierr)
+  fnameroot = setprefix(validfreqs, requested, nml_fh, nml_fnameprefix, errmsg, ierr)
 
-  is_passing = (ierr == 0 .and. trim(fnameroot(1)) == 'ocn_1h' .and. trim(fnameroot(2)) == 'ocn_3h')
+  is_passing = (ierr == 0 .and. trim(fnameroot(1)) == 'ocn_01h' .and. trim(fnameroot(2)) == 'ocn_03h')
   if (is_passing) then
      npass = npass + 1
      msg(nt) = trim(testname)//' PASS'
@@ -244,12 +243,12 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrootname: multi-request, only one rootname'
-  output_fh = (/1,3,0,0/)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setprefix: multi-request, only one rootname :'
+  nml_fh = (/1,3,0,0/)
   requested = (/ .true., .true., .false., .false. /)
-  output_rootname = (/ character(len=12) :: 'ocn_1h', '', '', '' /)
+  nml_fnameprefix = (/ character(len=12) :: 'ocn_01h', '', '', '' /)
 
-  fnameroot = setrootname(validfreqs, requested, output_fh, output_rootname, errmsg, ierr)
+  fnameroot = setprefix(validfreqs, requested, nml_fh, nml_fnameprefix, errmsg, ierr)
 
   is_passing = (ierr /= 0)
   if (is_passing) then
@@ -262,12 +261,12 @@ program test_outputlog_methods
 
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrootname: misaligned rootname on active slot fails strictly'
-  output_fh = (/1,24,0,0/)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setprefix: misaligned rootname on active slot fails :'
+  nml_fh = (/1,24,0,0/)
   requested = (/ .true., .false., .false., .true. /)
-  output_rootname = (/ character(len=12) :: 'ocn_1h', 'ocn_daily', '', '' /)
+  nml_fnameprefix = (/ character(len=12) :: 'ocn_1h', 'ocn_daily', '', '' /)
 
-  fnameroot = setrootname(validfreqs, requested, output_fh, output_rootname, errmsg, ierr)
+  fnameroot = setprefix(validfreqs, requested, nml_fh, nml_fnameprefix, errmsg, ierr)
 
   is_passing = (ierr /= 0)
   if (is_passing) then
@@ -280,15 +279,15 @@ program test_outputlog_methods
 #endif
   ! ------------------
   nt = nt + 1
-  write(testname,'(A,I2.2,A)')'test ',nt,' setrootname: specified rootname too long'
-  output_fh = (/6,0,0,0/)
+  write(testname,'(A,I2.2,A)')'test ',nt,' setprefix: specified rootname too long :'
+  nml_fh = (/6,0,0,0/)
   requested = (/ .false., .false., .true., .false. /)
   longrootname = 'rootname_too_long_and_is_truncated'
-  overlength_rootname(:) = ''
-  overlength_rootname(3) = longrootname
-  print *,overlength_rootname
+  overlength_fnameprefix(:) = ''
+  overlength_fnameprefix(3) = longrootname
+  print *,overlength_fnameprefix
 
-  fnameroot = setrootname(validfreqs, requested, output_fh, overlength_rootname, errmsg, ierr)
+  fnameroot = setprefix(validfreqs, requested, nml_fh, overlength_fnameprefix, errmsg, ierr)
 
   is_passing = (ierr /= 0)
   if (is_passing) then
@@ -302,13 +301,18 @@ program test_outputlog_methods
   ! ------------------
   ! Test results
   ! ------------------
+
   ntests = nt
-  !if (ntests > maxtests)
-  !do n = 1,ntests
-  !   print '(i4,A)',n,' '//trim(msg(n))
-  !enddo
-
-  !if (nfail /= 0) tests failed
-
+  if (ntests > maxtests) then
+     print '(A)', 'FAIL: ntests > maxtests '
+     stop 1
+  else
+     if (nfail == 0) then
+        print '(A)', 'All tests passed '
+     else
+        print '(A)', 'FAIL: At least one test failed '
+        stop 1
+     endif
+  endif
 
 end program test_outputlog_methods
