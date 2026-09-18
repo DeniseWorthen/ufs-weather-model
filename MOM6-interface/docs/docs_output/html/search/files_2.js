9f998cf13103ec60e8a3a26c6dadf425ba89b504
denise.worthen <Denise.Worthen@noaa.gov>
2026-09-18 08:20:32 -0500

    fix readnml header

--- MOM6-interface/docs/docs_output/html/search/files_2.js
+++ MOM6-interface/docs/docs_output/html/search/files_2.js
@@ -4,6 +4,7 @@ var searchData=
   ['test_5foutputlog_5falarminit_2ef90_1',['test_outputlog_alarminit.F90',['../test__outputlog__alarminit_8F90.html',1,'']]],
   ['test_5foutputlog_5fcompletion_2ef90_2',['test_outputlog_completion.F90',['../test__outputlog__completion_8F90.html',1,'']]],
   ['test_5foutputlog_5ffreqn_2ef90_3',['test_outputlog_freqn.F90',['../test__outputlog__freqn_8F90.html',1,'']]],
-  ['test_5foutputlog_5frestn_2ef90_4',['test_outputlog_restn.F90',['../test__outputlog__restn_8F90.html',1,'']]],
-  ['test_5futils_2ef90_5',['test_utils.F90',['../test__utils_8F90.html',1,'']]]
+  ['test_5foutputlog_5freadnml_2ef90_4',['test_outputlog_readnml.F90',['../test__outputlog__readnml_8F90.html',1,'']]],
+  ['test_5foutputlog_5frestn_2ef90_5',['test_outputlog_restn.F90',['../test__outputlog__restn_8F90.html',1,'']]],
+  ['test_5futils_2ef90_6',['test_utils.F90',['../test__utils_8F90.html',1,'']]]
 ];
